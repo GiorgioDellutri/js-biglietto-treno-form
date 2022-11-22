82a86469d3ba790b793fd4342d55cd2a14189f24
GiorgioDellutri <giorgiodll93@gmail.com>
2022-11-22 18:30:57 +0100

    recap info

--- js/script.js
+++ js/script.js
@@ -2,7 +2,11 @@
 const inputKm = document.getElementById('km');
 const inputAge = document.getElementById('age');
 const output = document.getElementById('prezzo');
+const travel = document.getElementById('travel');
+const price = document.getElementById('price');
+const discount = document.getElementById('discount');
 const button = document.querySelector('a.btn');
+const reset= document.querySelector('a.reset')
 
 button.addEventListener('click', function() {
 
@@ -16,14 +20,26 @@ button.addEventListener('click', function() {
         let scontoDiciassette = prezzo *17.5 / 100;
         output.innerHTML = 'Il costo del biglietto è: '+scontoDiciassette.toFixed(2) + '&euro;';
 
+        travel.innerHTML = 'La tua tratta: ' +inputKm.value + 'Km';
+        // price.innerHTML = 'Prezzo biglietto intero: ' +prezzo.toFixed(2) + '&euro;';
+        discount.innerHTML = 'Sconto applicato: ' + '17.5%';
+
     } else if (inputAge.value > 65) {
 
         let scontoTrenta = prezzo *33.3 / 100;
-        output.innerHTML= 'Il costo del biglietto è: ' +scontoTrenta.toFixed(2) + '&euro;';
+        output.innerHTML = 'Il costo del biglietto è: ' +scontoTrenta.toFixed(2) + '&euro;';
+        // price.innerHTML = 'Prezzo biglietto intero: ' +prezzo.toFixed(2) + '&euro;';
+        travel.innerHTML = 'La tua tratta: ' +inputKm.value + 'Km';
+        discount.innerHTML = 'Sconto applicato: ' + '33.3%';
 
     } else{
         output.innerHTML='Il costo del biglietto è: ' +prezzo.toFixed(2) + '&euro;';
-    }
-    console.log(prezzo)
 
+        travel.innerHTML = 'La tua tratta: ' +inputKm.value + 'Km';
+    }
+
+})
+
+reset.addEventListener('click', function() {
+    inputAge.innerHTML = " ";
 })
