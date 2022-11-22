fa8fead5b6b5f8c6fd88582c8ff60b1c46af48b9
GiorgioDellutri <giorgiodll93@gmail.com>
2022-11-22 20:13:12 +0100

    homepage

--- js/script.js
+++ js/script.js
@@ -21,20 +21,19 @@ button.addEventListener('click', function() {
         output.innerHTML = 'Il costo del biglietto è: '+scontoDiciassette.toFixed(2) + '&euro;';
 
         travel.innerHTML = 'La tua tratta: ' +inputKm.value + 'Km';
-        // price.innerHTML = 'Prezzo biglietto intero: ' +prezzo.toFixed(2) + '&euro;';
+        price.innerHTML = 'Prezzo biglietto intero: ' +prezzo.toFixed(2) + '&euro;';
         discount.innerHTML = 'Sconto applicato: ' + '17.5%';
 
     } else if (inputAge.value > 65) {
 
         let scontoTrenta = prezzo *33.3 / 100;
         output.innerHTML = 'Il costo del biglietto è: ' +scontoTrenta.toFixed(2) + '&euro;';
-        // price.innerHTML = 'Prezzo biglietto intero: ' +prezzo.toFixed(2) + '&euro;';
         travel.innerHTML = 'La tua tratta: ' +inputKm.value + 'Km';
+        price.innerHTML = 'Prezzo biglietto intero: ' +prezzo.toFixed(2) + '&euro;';
         discount.innerHTML = 'Sconto applicato: ' + '33.3%';
 
     } else{
         output.innerHTML='Il costo del biglietto è: ' +prezzo.toFixed(2) + '&euro;';
-
         travel.innerHTML = 'La tua tratta: ' +inputKm.value + 'Km';
     }
 
@@ -43,4 +42,8 @@ button.addEventListener('click', function() {
 reset.addEventListener('click', function() {
     inputAge.value = " ";
     inputKm.value = " ";
+    travel.innerHTML = ' ';
+    price.innerHTML = '';
+    discount.innerHTML = '';
+    output.innerHTML='';
 })
