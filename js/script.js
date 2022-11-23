986623a1092c5e9dbd1cab994a5f288652f63f3f
GiorgioDellutri <giorgiodll93@gmail.com>
2022-11-23 11:01:11 +0100

    fixed value km / age

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
-const inputKm = document.getElementById('km');
-const inputAge = document.getElementById('age');
+const kmElement = document.getElementById('km');
+const ageElement = document.getElementById('age');
 const output = document.getElementById('prezzo');
 const travel = document.getElementById('travel');
 const price = document.getElementById('price');
@@ -10,38 +10,40 @@ const reset= document.querySelector('a.reset')
 
 button.addEventListener('click', function() {
 
-    console.log(parseInt(inputKm.value));
-    console.log(parseInt(inputAge.value));
-    
-    let prezzo = (0.2762 * inputKm.value);
+    const km = parseInt(kmElement.value)
+    const age = parseInt(ageElement.value)
 
-    if (inputAge.value < 18) {
+    // Aggiungere const di input value con parseint
+    
+    let prezzo = (0.2762 * kmElement.value);
+
+    if (ageElement.value < 18) {
 
         let scontoDiciassette = prezzo *17.5 / 100;
         output.innerHTML = 'Il costo del biglietto è: '+scontoDiciassette.toFixed(2) + '&euro;';
 
-        travel.innerHTML = 'La tua tratta: ' +inputKm.value + 'Km';
+        travel.innerHTML = 'La tua tratta: ' +kmElement.value + 'Km';
         price.innerHTML = 'Prezzo biglietto intero: ' +prezzo.toFixed(2) + '&euro;';
         discount.innerHTML = 'Sconto applicato: ' + '17.5%';
 
-    } else if (inputAge.value > 65) {
+    } else if (ageElement.value > 65) {
 
         let scontoTrenta = prezzo *33.3 / 100;
         output.innerHTML = 'Il costo del biglietto è: ' +scontoTrenta.toFixed(2) + '&euro;';
-        travel.innerHTML = 'La tua tratta: ' +inputKm.value + 'Km';
+        travel.innerHTML = 'La tua tratta: ' +kmElement.value + 'Km';
         price.innerHTML = 'Prezzo biglietto intero: ' +prezzo.toFixed(2) + '&euro;';
         discount.innerHTML = 'Sconto applicato: ' + '33.3%';
 
     } else{
         output.innerHTML='Il costo del biglietto è: ' +prezzo.toFixed(2) + '&euro;';
-        travel.innerHTML = 'La tua tratta: ' +inputKm.value + 'Km';
+        travel.innerHTML = 'La tua tratta: ' +kmElement.value + 'Km';
     }
 
 })
 
 reset.addEventListener('click', function() {
-    inputAge.value = " ";
-    inputKm.value = " ";
+    ageElement.value = " ";
+    kmElement.value = " ";
     travel.innerHTML = ' ';
     price.innerHTML = '';
     discount.innerHTML = '';
